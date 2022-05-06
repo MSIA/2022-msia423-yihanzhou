2ed5555b6392768abe5c7363ddd51027ba54d5e4
Franklin990218 <yihanzhou2023@u.northwestern.edu>
2022-05-06 16:24:33 -0500

    Updated the information for database connection

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -2,12 +2,23 @@ import os
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
 PORT = 5000
-APP_NAME = "penny-lane"
+APP_NAME = "bmw_price_estimator"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
 MAX_ROWS_SHOW = 100
 
+DB_HOST = os.environ.get('MYSQL_HOST')
+DB_PORT = os.environ.get('MYSQL_PORT')
+DB_USER = os.environ.get('MYSQL_USER')
+DB_PW = os.environ.get('MYSQL_PASSWORD')
+DATABASE = os.environ.get('DATABASE_NAME')
+DB_DIALECT = 'mysql+pymysql'
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-if SQLALCHEMY_DATABASE_URI is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
+if SQLALCHEMY_DATABASE_URI is not None:
+    pass
+elif DB_HOST is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/msia423_db.db'
+else:
+    SQLALCHEMY_DATABASE_URI = f'{DB_DIALECT}://{DB_USER}:{DB_PW}@{DB_HOST}:{DB_PORT}/{DATABASE}'
+
